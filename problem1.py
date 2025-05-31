89819fc567f6ba5a265edff8ff29d22f2dde3eb4
Jerrick <jermil2990@gmail.com>
2025-05-31 01:40:56 -0500

    Problem 1-Hundred Acre Wood
    Developed function called "welcome" which prints a string text after the function is called

---
+++ problem1.py
@@ -0,0 +1,11 @@
+# Problem 1: Hundred Acre Wood
+# Write a function welcome() that prints the string "Welcome to The Hundred Acre Wood!"
+
+# Create function welcome() 
+# Print string 
+# Call function welcome()
+
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+welcome()
