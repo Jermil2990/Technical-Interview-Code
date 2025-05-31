e6be3b3e4091dce4bb945a9db2793bfda65f96ec
Jerrick <jermil2990@gmail.com>
2025-05-31 01:53:13 -0500

    Problem 2-Greeting
    Created a function called "greeting" that takes in a name and prints a message with the name included!

---
+++ problem2.py
@@ -0,0 +1,14 @@
+# Problem 2: Greeting
+# Write a function greeting() that accepts a single parameter, a string name, and prints the string "Welcome to The Hundred Acre Wood <name>! My name is Christopher Robin."
+
+# Create function greeting - parameter (string name)
+# Print text with string included    
+# Call function to display to editor 
+
+# Tips** Formated Strings : print(f"Welcome to Codepath, {name}!") # Prints 'Welcome to CodePath, Michael!'
+
+def greeting(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.")
+
+greeting("Micheal")
+    
