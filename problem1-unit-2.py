e3fe2d8650985296d6e406d7422b2c2fe2ba5765
Jerrick <jermil2990@gmail.com>
2025-05-31 02:26:56 -0500

    Set up Problem 2 Branch
    Created problem description to test branch movement

---
+++ problem1-unit-2.py
@@ -0,0 +1,3 @@
+# Problem 1: Festival Lineup
+# Given two lists of strings artists and set_times of length n, write a function lineup() that maps each artist to their set time.
+# An artist artists[i] has set time set_times[i]. Assume i <= 0 < n and len(artists) == len(set_times).
